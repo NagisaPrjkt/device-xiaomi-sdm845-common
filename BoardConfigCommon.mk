1217a4282f3eb8192368caecefcd376a82de06ff
Shikhar <krtonia@gmail.com>
2024-06-24 19:22:06 +0530

    sdm845-common: Register missing HALS in FCM
    The following HALs in device manifest are not declared in FCM <= level 5:
      android.hardware.biometrics.fingerprint@2.3::IBiometricsFingerprint/default
      android.hardware.bluetooth.audio@2.1::IBluetoothAudioProvidersFactory/default
      android.hardware.cas.IMediaCasService/default (@1)
      android.hardware.drm.IDrmFactory/clearkey (@1)
      android.hardware.light@2.0::ILight/default
      android.hardware.radio.config@1.2::IRadioConfig/default
      android.hardware.usb@1.3::IUsb/default
      android.hardware.vibrator@1.0::IVibrator/default
      android.hardware.wifi.IWifi/default (@2)
      android.hardware.wifi.hostapd.IHostapd/default (@2)
      android.hardware.wifi.supplicant.ISupplicant/default (@3)
    INCOMPATIBLE
    
    Signed-off-by: Shikhar <krtonia@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -76,6 +76,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(COMMON_PATH)/framework_compatibility_matrix.xml
 
 # LMKD
 TARGET_LMKD_STATS_LOG := true
